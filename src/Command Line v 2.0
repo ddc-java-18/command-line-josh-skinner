bb73f0c262d74e607eb61f6755e46fafc56ff002
jskinner <jskinner18@cnm.edu>
2024-05-20 12:01:48 -0600

    Fahrenheit-to-Celsius conversion

---
+++ src/Command Line v 2.0
@@ -0,0 +1,20 @@
+public class CommandLine {
+
+	public static void main(String[] args) {
+		
+		for (String arg : args) {
+		
+		System.out.println(arg);
+		
+		}
+		
+		
+	 
+	} 
+
+	private static double convertF2C (double fahrenheit) {
+		return 5 / 9 * (fahrenheit -32)
+	
+	}
+
+}
--- src/CommandLine.java
+++ src/CommandLine.java
@@ -3,13 +3,19 @@ public class CommandLine {
 	public static void main(String[] args) {
 		
 		for (String arg : args) {
-		
-		System.out.println(arg);
+		double fahrenheit = Double.parseDouble(arg);
+		double celsius = convertF2C (fahrenheit) ;
+		System.out.printf("F = %.2f; C = %.2f%n", fahrenheit, celsius);
 		
 		}
 		
 		
 	 
 	} 
+
+	private static double convertF2C (double fahrenheit) {
+		return (fahrenheit -32) * 5 / 9;
+	
+	}
 
 }
